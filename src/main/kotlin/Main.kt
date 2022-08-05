678e278169a6b3f3da0f070ed7d7a99677cdfc12
israel <israelrezendeluiz2903@gmail.com>
2022-08-05 16:00:57 -0300

    Iniciando classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,38 @@
 fun main() {
     println("Bem vindo ao ByteBank")
     println()
+
+    val contaRael = Conta()
+    contaRael.titular = "Rael RZD"
+    contaRael.numero = 1000
+    contaRael.saldo = 1000.0
+
+    val contaGui = Conta()
+    contaGui.titular = "Gui Paizao"
+    contaGui.numero = 1001
+    contaGui.saldo = 1500.0
+
+    println(contaRael.titular)
+    println(contaRael.numero)
+    println(contaRael.saldo)
+
+    println()
+
+    println(contaGui.titular)
+    println(contaGui.numero)
+    println(contaGui.saldo)
+
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+
+
+fun testaLacos() {
     var i = 1
 
     while (i <= 5) {
@@ -15,7 +47,6 @@ fun main() {
         println()
         i++
     }
-
 }
 
 fun testaCondicoes(saldo: Double) {

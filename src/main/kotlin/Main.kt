6fc0721714ca1325b5a423bccbb9beab74271f39
israel <israelrezendeluiz2903@gmail.com>
2022-08-08 15:45:09 -0300

    Ajustado class funcionario, incluido class gerente

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,13 +6,29 @@ fun main() {
         nome = "Israel",
         cpf = "123.456.789-00",
         salario = 1000.00,
-        tipo = 1
+    )
+
+    val teteu = Gerente(
+        nome = "Teteu", cpf = "111.222.333.44", salario = 2000.00, senha = 1234
     )
 
     println("Funcionário: ${israel.nome}")
     println("CPF: ${israel.cpf}")
     println("Salário: ${israel.salario}")
     println("Bonificação: ${israel.bonificacao()}")
+
+    println()
+
+    println("Funcionário: ${teteu.nome}")
+    println("CPF: ${teteu.cpf}")
+    println("Salário: ${teteu.salario}")
+    println("Bonificação: ${teteu.bonificacao()}")
+
+    if (teteu.autentica(senha = 1234)) {
+        println("Autenticação realizada com sucesso!")
+    } else {
+        println("A autenticação falhou!")
+    }
 
 
 }

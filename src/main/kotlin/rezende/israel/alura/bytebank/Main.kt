f43fafd82986504a2683ecc90c7805fe017a51fa
israel <israelrezendeluiz2903@gmail.com>
2022-08-15 17:27:08 -0300

    realizado testes em relação a busca de valores em lista utilizando any, all, filter, find

--- src/main/kotlin/rezende/israel/alura/bytebank/Main.kt
+++ src/main/kotlin/rezende/israel/alura/bytebank/Main.kt
@@ -1,5 +1,10 @@
+import rezende.israel.alura.bytebank.modelo.ContaSalario
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 fun main() {
 
+    //VERIFICAÇÃO DE MAIORES E MENORES IDADES
     val idades = arrayListOf<Int>(33, 42, 20, 15, 26)
     var maiorIdade = Int.MIN_VALUE
     var menorIdade = Int.MAX_VALUE
@@ -15,17 +20,42 @@ fun main() {
     println(maiorIdade)
     println(menorIdade)
 
+    println("==================================================")
 
+    //TESTE VERIFICAÇÕES EM LISTA, FILTER (RETORNA V OU F SE UM NUMERO EXISTE NA LISTA), FIND (RETORNA O NUMERO INSERIDO NA BUSCA OU NULL), ALL E ANY
+    val salariosTeste = arrayListOf<Double>(2000.00, 2500.00, 3000.00, 4500.00)
+    val menores: Double? = salariosTeste.find { it == 3300.0 }
+    println(menores)
 
-    val salarios = arrayListOf<Double>(1500.0, 2500.0, 4200.0, 5300.0, 10000.0)
-    val aumento = 1.1
+    println("==================================================")
 
-    salarios.forEachIndexed { i, salario ->
-        salarios[i] = salario * aumento
-    }
+    //CALCULO DE 10% AUMENTO DE SALÁRIO COM AUMENTO MINIMO DE R$500
+    val salarios = bigDecimalArrayListOf("1500.00", "2500.00", "3000.00", "3500.00", "6500.00")
+    val aumento = "1.1".toBigDecimal()
+    val salariosComAumento: Array<BigDecimal> = salarios.map { salario ->
+        calculaAumentoRelativo(salario, aumento)
+    }.toTypedArray()
 
-    println(salarios.toString())
+    println(salarios.contentToString())
+    println(salariosComAumento.contentToString())
+
 
 }
+
+
+fun bigDecimalArrayListOf(vararg valores: String): Array<BigDecimal> {
+    return Array<BigDecimal>(valores.size) { i ->
+        valores[i].toBigDecimal()
+    }
+}
+
+fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal): BigDecimal {
+    return if (salario < "5000.00".toBigDecimal()) {
+        salario + "500.00".toBigDecimal()
+    } else {
+        (salario * aumento).setScale(2, RoundingMode.UP)
+    }
+}
+
 
 

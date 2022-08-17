68b78b8ee910033f6f2e0fc1541e4d492dbd3f21
israel <israelrezendeluiz2903@gmail.com>
2022-08-17 17:55:38 -0300

    realizado testes na coleção map utilizando os metodos de association e group para filtragem de pedidos com frete gratis e criação de agendas com filtros

--- src/main/kotlin/rezende/israel/alura/Main.kt
+++ src/main/kotlin/rezende/israel/alura/Main.kt
@@ -1,10 +1,52 @@
-import rezende.israel.alura.cursoCollections.testes.testeMap
+import kotlin.jvm.internal.PropertyReference0Impl
 
 fun main() {
-    testeMap()
+
+    val pedidos = listOf(
+        Pedido(1, 20.0),
+        Pedido(2, 30.0),
+        Pedido(3, 40.0),
+        Pedido(4, 50.0)
+    )
+
+    val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido ->
+        pedido.numero
+    }
+
+
+//    val pedidosComFreteGratis: Map<Pedido, Boolean> = pedidos.associateWith { Pedido ->
+//        Pedido.valor > 30.0
+//    }
+
+    val pedidosComFreteGratis = pedidos.groupBy { pedido: Pedido ->
+        pedido.valor >= 40
+    }
+
+
+    println(pedidosMapeados)
+    println(pedidosComFreteGratis[true])
+
+
+    val nomes = listOf<String>(
+        "Rael",
+        "Dan",
+        "Gui",
+        "Ga"
+    )
+
+    val agenda = nomes.groupBy { nome ->
+        nome.first()
+    }
+
+    println(agenda)
+    println(agenda['R'])
 
 
 }
 
 
+data class Pedido(
+    val numero: Int,
+    val valor: Double
+)
 

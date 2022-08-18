67a831229bc805e9fcf9ace2ca61143e2557b9bd
israel <israelrezendeluiz2903@gmail.com>
2022-08-18 10:50:58 -0300

    variaveis funcionais alteradas para a operação de soma

--- src/main/kotlin/rezende/israel/alura/Main.kt
+++ src/main/kotlin/rezende/israel/alura/Main.kt
@@ -1,22 +1,36 @@
-import rezende.israel.alura.cursoCollections.testes.testeOrganizacaoMap
-
 fun main() {
-    val minhaFuncao: () -> Unit = ::teste
-    println(minhaFuncao())
 
+    //VARIAVEL FUNCIONAL A PARTIR DE FUNÇÃO
+    val minhaFuncao: (Int, Int) -> Int = ::teste
+    println(minhaFuncao(10, 15))
 
-    val minhaFuncaoClasse: () -> Unit = Teste()
-    println(minhaFuncaoClasse())
+    //VARIAVEL FUNCIONAL A PARTIR DE CLASSE (INVOKE)
+    val minhaFuncaoClasse: (Int, Int) -> Int = Teste()
+    println(minhaFuncaoClasse(5, 10))
+
+    //VARIAVEL FUNCIONAL LAMBDA
+    val minhaFuncaoLambda: () -> Unit = {
+        println("Executando como lambda")
+    }
+
+    //VARIAVEL FUNCIONAL ANONIMA
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Executa como anonima")
+    }
+
+    println(minhaFuncaoLambda())
+    println(minhaFuncaoAnonima())
+
 
 }
 
-fun teste() {
-    println("Executado teste")
+fun teste(a: Int, b: Int): Int {
+    return a + b
 }
 
-class Teste : () -> Unit {
-    override fun invoke() {
-        println("Executado invoke de Teste")
+class Teste : (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int {
+        return a + b
     }
 
 }

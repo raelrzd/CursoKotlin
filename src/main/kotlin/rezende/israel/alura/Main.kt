41fdb30f73b2e9ec4b179dbd9678fc47bbb90a7f
israel <israelrezendeluiz2903@gmail.com>
2022-08-18 11:48:20 -0300

    incluido as variaveis funcionais do tipo lambda e anonima para o calculo de bonificação de salarios

--- src/main/kotlin/rezende/israel/alura/Main.kt
+++ src/main/kotlin/rezende/israel/alura/Main.kt
@@ -1,3 +1,5 @@
+import java.util.zip.DeflaterOutputStream
+
 fun main() {
 
     //VARIAVEL FUNCIONAL A PARTIR DE FUNÇÃO
@@ -22,7 +24,27 @@ fun main() {
     println(minhaFuncaoAnonima(20, 15))
 
 
+    val calculadoraBonificacao: (salario: Double) -> Double = lambda@{ salario ->
+        if (salario > 1000.0) {
+            return@lambda salario + 50.0
+        }
+        salario + 100.0
+    }
+
+    val calculadoraBonificacaoAnonima: (Double) -> Double = fun(salario: Double): Double {
+        if (salario > 1000.0) {
+            return salario + 50.0
+        }
+        return salario + 100.0
+    }
+
+    println(calculadoraBonificacao(1000.0))
+    println(calculadoraBonificacaoAnonima(1100.0))
+
+
 }
+
+
 
 fun teste(a: Int, b: Int): Int {
     return a + b

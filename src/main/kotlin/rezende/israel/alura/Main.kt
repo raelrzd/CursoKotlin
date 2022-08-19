4c0d747410809594e1a6cfc712f5786b2ecaa9a7
israel <israelrezendeluiz2903@gmail.com>
2022-08-19 10:04:18 -0300

    incluido metodo via let para aplicar letras maiusculas em itens de lista, tambem incluido metodo para filtro de complemento

--- src/main/kotlin/rezende/israel/alura/Main.kt
+++ src/main/kotlin/rezende/israel/alura/Main.kt
@@ -1,32 +1,26 @@
 import rezende.israel.alura.bytebank.modelo.Endereco
+import java.util.*
+import kotlin.math.E
+import kotlin.math.log
 
 fun main() {
 
-   val testaFuncao: () -> Unit
-    Endereco().let {
-        it
+
+    Endereco(logradouro = "rua santana", numero = 333).let { endereco ->
+        val enderecoMaisculo: String = "${endereco.logradouro}, ${endereco.numero}".uppercase()
+        println(enderecoMaisculo)
     }
 
-    testaFuncao.let {
-        it
-    }
+    listOf(
+        Endereco(logradouro = "Rua Santana", numero = 333, complemento = "casa"),
+        Endereco(logradouro = "Av presidente vargas", numero = 444)).filter { endereco ->
+        endereco.complemento.isNotEmpty()
+    }.let ( ::println)
 
-    "".let { ::testaString }
+    
 
-    1.let{
-        it
-    }
-
-    teste(20, {})
-
-}
-
-fun testaString(valor: String) {
+//    val endereco = Endereco(logradouro = "rua vergueiro", numero = 3185)
+//    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".uppercase(Locale.getDefault())
+//    println(enderecoEmMaiusculo)
 
 }
-
-
-fun teste(valor: Int, bloco: () -> Unit){
-
-}
-
